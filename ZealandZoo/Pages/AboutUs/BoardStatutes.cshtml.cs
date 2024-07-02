f4ec84622479050b4c90a2d123d10c324b3508d9
Kevin Waweru <kew001@edu.zealand.dk>
2024-07-02 15:36:58 +0200

    Navbar lavet

---
+++ ZealandZoo/Pages/AboutUs/BoardStatutes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ZealandZoo.Pages.AboutUs
+{
+    public class BoardStatutesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ZealandZoo/Pages/AboutUs/PartyCommittee.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ZealandZoo.Pages.AboutUs
+{
+    public class PartyCommitteeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
